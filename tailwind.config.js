037ce207d039b5671886a1653f407c3180aa43a5
krishna jain <krishnajain5050@gmail.com>
2024-02-26 16:03:24 +0530

    feat: added tailwind colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      colors: {
+        white: "#fff",
+        lightskyblue: "#8eb6f8",
+        dodgerblue: "#4285f4",
+        gainsboro: "#d9d9d9",
+        black: "#000",
+        darkgray: "#979494",
+        gray: "rgba(0, 0, 0, 0.4)",
+        whitesmoke: "#f9f2f2",
+      },
+      fontFamily: {
+        phetsarath: "Phetsarath",
+        "open-sans": "'Open Sans'",
+        oswald: "Oswald",
+        poppins: "Poppins",
+        "dm-sans": "'DM Sans'",
+      },
+    },
+    plugins: [],
+  }
 }
 
